e000de123cb6301e77b51d36bf00cfc7820426d7
Ak9994 <163250688+Ak9994@users.noreply.github.com>
2024-09-12 15:35:01 +0530

    Update main.cpp

--- C-Basics-Assignment/main.cpp
+++ C-Basics-Assignment/main.cpp
@@ -4,16 +4,9 @@
 #include "DataHandling.h"
 #include "Conditional.h"
 #include "atk.h"
+#include "functions.h"
 
 int main() {
-  //Basic Display Method
-  //display();
-  //Datatype size Method
-  //std::cout << "displaying output of size: \n" << size() << "\n";
-  //Data Handling method
-  //DataHandling();
-  //Conditional method
-  //Check();
-  //Basic conditional AI result check
-  atk();
+  //Call Method from file
+  
 }
